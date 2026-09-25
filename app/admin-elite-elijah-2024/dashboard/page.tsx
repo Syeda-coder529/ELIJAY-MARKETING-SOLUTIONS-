25705a0d15d4c8498588e3eeca0d0651248759eb
Syeda-coder529 <syalizaazhar54@gmail.com>
2026-09-25 11:25:23 +0500

    Delete app directory

--- app/admin-elite-elijah-2024/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import { DashboardShell } from "@/components/admin/dashboard-shell";
-
-export const dynamic = "force-dynamic";
-
-export default function AdminDashboardPage() {
-  return <DashboardShell />;
-}
